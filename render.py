474ddc44356f2b8d80ac3576af5354df64f3eb26
Koen van Eijk <vaneijk.koen@gmail.com>
2024-05-02 09:16:33 +0200

    added canonical urls for SEO

--- render.py
+++ render.py
@@ -16,16 +16,18 @@ os.mkdir("./static")
 # render about us page, create about-us folder
 os.mkdir("./static/about-us")
 template = template_env.get_template("about_us.html")
+url = "https://magifind.com/about-us/"
 with open("./static/about-us/index.html", "w", encoding="utf-8") as f:
-    f.write(template.render())
-urls.append("https://magifind.com/about-us/")
+    f.write(template.render(canonical_url=url))
+urls.append(url)
 
+# render investors page, create investors folder
 os.mkdir("./static/investors")
 template = template_env.get_template("investors.html")
+url = "https://magifind.com/investors/"
 with open("./static/investors/index.html", "w", encoding="utf-8") as f:
-    f.write(template.render())
-urls.append("https://magifind.com/investors/")
-
+    f.write(template.render(canonical_url=url))
+urls.append(url)
 # render landing pages from landing_pages.yml
 with open("./landing_pages.yml", "r") as f:
     landing_pages = yaml.safe_load(f)
@@ -34,17 +36,19 @@ for landing_page in landing_pages:
     slug = landing_page["slug"]
     os.mkdir("./static/" + slug)
     template = template_env.get_template("landing_page.html")
+    url = "https://magifind.com/" + slug + "/"
     with open("./static/" + slug + "/index.html", "w", encoding="utf-8") as f:
-        f.write(template.render(landing_page=landing_page))
-    urls.append("https://magifind.com/" + slug + "/")
+        f.write(template.render(landing_page=landing_page, canonical_url=url))
+    urls.append(url)
 
 # render ai_search_test.html
 template = template_env.get_template("ai_search_test.html")
 # create folder
 os.mkdir("./static/ai-search-test")
+url = "https://magifind.com/ai-search-test/"
 with open("./static/ai-search-test/index.html", "w", encoding="utf-8") as f:
-    f.write(template.render())
-urls.append("https://magifind.com/ai-search-test/")
+    f.write(template.render(canonical_url=url))
+urls.append(url)
 
 # Blog
 with open("./blog.yml", "r") as f:
@@ -55,22 +59,25 @@ for post in blog["posts"]:
     os.mkdir("./static/blog/" + slug)
     post['image'] = post['image'].replace('.webp', '.svg')
     template = template_env.get_template("post.html")
+    url = "https://magifind.com/blog/" + slug + "/"
     with open("./static/blog/" + slug + "/index.html", "w", encoding="utf-8") as f:
-        f.write(template.render(post=post))
+        f.write(template.render(post=post, canonical_url=url))
 
     shutil.copyfile(
         "./images/" + post["image"], "./static/blog/" + slug + "/" + post["image"]
     )
 template = template_env.get_template("blog.html")
+url = "https://magifind.com/blog/"
 with open("./static/blog/index.html", "w", encoding="utf-8") as f:
-    f.write(template.render(blog=blog))
+    f.write(template.render(blog=blog, canonical_url=url))
 
 # Home
 with open("./faq.yml", "r") as f:
     faq = yaml.safe_load(f)
 template = template_env.get_template("home.html")
+url = "https://magifind.com/"
 with open("./static/index.html", "w", encoding="utf-8") as f:
-    f.write(template.render(blog=blog, faq=faq, posts=blog["posts"]))
+    f.write(template.render(blog=blog, faq=faq, posts=blog["posts"], canonical_url=url))
 
 # copy all files from ./images to ./static
 shutil.copytree("./images", "./static/images")
